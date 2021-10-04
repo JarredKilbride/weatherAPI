8c5d2a50c57fc06d83084d407c36cc7164092b39
JarredGriffith <Jarredgriffith29@gmail.com>
2021-10-04 12:01:38 -0700

    fixing issues with the cors proxy

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ window.addEventListener('load',()=> {
             lat = po.coords.latitude;
            
             const proxy = `https://cors-anywhere.herokuapp.com/`
-            const api = `${proxy}http://api.weatherapi.com/v1/current.json?key=58a5245caa7f4d3dbc603847210310&q=${lat},${long}&aqi=no`
+            const api = `http://api.weatherapi.com/v1/current.json?key=58a5245caa7f4d3dbc603847210310&q=${lat},${long}&aqi=no`
 
             fetch(api)
                 .then(resp => {
